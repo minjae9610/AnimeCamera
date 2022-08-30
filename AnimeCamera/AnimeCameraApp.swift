b4a5b88b2625097d9d4d1512bb7ebf96e4faea52
minjae Kim <hjn966@naver.com>
2022-08-30 15:25:51 +0900

    initial commit

---
+++ AnimeCamera/AnimeCameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimeCameraApp.swift
+//  AnimeCamera
+//
+//  Created by 김민재 on 2022/08/30.
+//
+
+import SwiftUI
+
+@main
+struct AnimeCameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
